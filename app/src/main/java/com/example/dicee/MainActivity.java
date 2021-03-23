23a19c6c38cc03b983fd5314c232d8a8707688f0
Kramik <kramiknakrani100@gmail.com>
2021-03-23 17:06:26 +0530

    Dicee App By Me

---
+++ app/src/main/java/com/example/dicee/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.dicee;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+        Button rollbutton;
+        rollbutton = (Button) findViewById(R.id.roll);
+
+        final ImageView leftdice = (ImageView) findViewById(R.id.Image_leftdice);
+        final ImageView rightdice = (ImageView) findViewById(R.id.Image_rightdice);
+
+        final int[] Dicearray =  {
+                R.drawable.dice1,
+                R.drawable.dice2,
+                R.drawable.dice3,
+                R.drawable.dice4,
+                R.drawable.dice5,
+                R.drawable.dice6
+        };
+
+        rollbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Rolling Done!", Toast.LENGTH_SHORT).show();
+
+                Random Randomnumbergenrator = new Random();
+                int number = Randomnumbergenrator.nextInt(6);
+
+                leftdice.setImageResource(Dicearray[number]);
+
+                number = Randomnumbergenrator.nextInt(6);
+                rightdice.setImageResource(Dicearray[number]);
+            }
+        });
+
+    }
+}
